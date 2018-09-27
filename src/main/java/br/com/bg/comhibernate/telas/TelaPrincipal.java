90f7a735c22f00463f56edaf8082ef97efc1038c
rubens leme <rubenss.leme@gmail.com>
2018-09-27 15:57:01 -0300

    Ajuste do código
    Ajuste do código
    
    Co-Authored-By: Carlos Henrique da Silva Kafka <carloskafka@users.noreply.github.com>

--- src/main/java/br/com/bg/comhibernate/telas/TelaPrincipal.java
+++ src/main/java/br/com/bg/comhibernate/telas/TelaPrincipal.java
@@ -1,13 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.bg.comhibernate.telas;
 
 /**
  *
- * @author laramara
+ * @author Rubens Leme
  */
 public class TelaPrincipal extends javax.swing.JFrame {
 
